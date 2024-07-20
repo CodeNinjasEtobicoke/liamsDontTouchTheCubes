95438db293a1f6397dcc67ffd7f25f01c996e0a0
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-07-20 12:19:36 -0400

    FINISHED

---
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    [Header("Destruction Timer")]
+    public float timeToDestruction;
+
+
+     void Start()
+    {
+        Invoke("DestroyObject", timeToDestruction);
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
+
